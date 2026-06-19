dccf125ce405cd1ea5d0d89f1f70bf7b171297ee
davi2008 <ldavifilipe@gmail.com>
2026-06-19 19:11:43 -0300

    fix: corrigir regex do CORS para dominios Vercel

--- backend_laravel/config/cors.php
+++ backend_laravel/config/cors.php
@@ -4,7 +4,7 @@ return [
     'paths'                    => ['api/*'],
     'allowed_methods'          => ['*'],
     'allowed_origins'          => [env('FRONTEND_URL', 'http://localhost:5173')],
-    'allowed_origins_patterns' => ['https://*.vercel.app'],
+    'allowed_origins_patterns' => ['#^https://.*\.vercel\.app$#'],
     'allowed_headers'          => ['*'],
     'exposed_headers'          => [],
     'max_age'                  => 0,
